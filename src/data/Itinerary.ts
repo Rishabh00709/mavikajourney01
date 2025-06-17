1e8eec6d729424190f802bef1e443ef6af8fcf3f
Rishabh00709 <rishabhjain.gfg@gmail.com>
2025-06-17 14:07:27 +0530

    changes on poackages page

---
+++ src/data/Itinerary.ts
@@ -0,0 +1,37 @@
+export interface ItineraryInterface {
+    id: string;
+    title: string;
+    description: string;
+    packageId:string;
+  }
+
+
+  export const itinerary: ItineraryInterface[] = [
+    {
+        id: "1",
+        title: "TEST",
+          
+        description: "loream WAS JHBDSC",
+        packageId:'cultural-varanasi'
+        
+    },
+
+    {
+        id: "1",
+        title: "TEST1",
+          
+        description: "loream ",
+        packageId:'cultural-varanasi'
+        
+    },
+
+
+    {
+        id: "2",
+        title: "TEST1",
+          
+        description: "loreamRISHIKESH ",
+        packageId:'cultural-haridwar-rishikesh'
+        
+    },
+  ]
